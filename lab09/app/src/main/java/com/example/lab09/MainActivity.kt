29c622b3db166ec7299859ffe1cba48503baa9e6
phuongntm08 <nguyenthiminhphuong02@gmail.com>
2021-11-10 17:57:24 +0700

    lab 09

---
+++ lab09/app/src/main/java/com/example/lab09/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.lab09
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var myToggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var drawerLayout: DrawerLayout = findViewById(R.id.myDrawerLayout)
+        var navView: NavigationView = findViewById(R.id.myDrawerView)
+
+        myToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
+
+        myDrawerLayout.addDrawerListener(myToggle)
+
+        myToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.sentMailId -> {
+                    Snackbar.make(findViewById(R.id.sentMailId),"Your Email has been sent successfully",
+                        Snackbar.LENGTH_LONG).show()
+                }
+                R.id.outboxId -> {
+                    Snackbar.make(findViewById(R.id.outboxId),"This is your OutBox folder",Snackbar.LENGTH_LONG).show()
+                }
+                R.id.spamId -> {
+                    Snackbar.make(findViewById(R.id.spamId),"This is your Spam folder",Snackbar.LENGTH_LONG).show()
+                }
+                R.id.calendarId -> {
+                    Snackbar.make(findViewById(R.id.calendarId),"This is your Calendar",Snackbar.LENGTH_LONG).show()
+                }
+                R.id.contactsId -> {
+                    startActivity(Intent(this, Contacts::class.java))}
+            }
+            true
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (myToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
